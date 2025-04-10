d4099207d3535eaa326f396dea303ce24b4b66da
Nobuhiro Ito <iseebi@iseteki.net>
2025-04-10 14:38:06 +0900

    Add Example

---
+++ Example/AizomeExample/AizomeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AizomeExampleApp.swift
+//  AizomeExample
+//
+//  Created by Nobuhiro Ito on 2025/04/10.
+//
+
+import SwiftUI
+
+@main
+struct AizomeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/AizomeExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AizomeExample
+//
+//  Created by Nobuhiro Ito on 2025/04/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
