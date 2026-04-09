024c64be90287e49c16450f5f6bd1eb9e1397b71
Nobuhiro Ito <iseebi@iseteki.net>
2026-04-09 23:35:24 +0900

    fix build error in macOS

--- Sources/AizomeUIKit/InteropBasicStringStyle.swift
+++ Sources/AizomeUIKit/InteropBasicStringStyle.swift
@@ -1,3 +1,4 @@
+#if canImport(UIKit)
 import SwiftUI
 import UIKit
 import Aizome
@@ -45,3 +46,4 @@ public struct InteropBasicStringStyle: AizomeStringStyle {
         attributed[range].mergeAttributes(container)
     }
 }
+#endif
